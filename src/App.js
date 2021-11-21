b9e68c435f188962c828390c4fcf738a18200d9a
danygza <dany.gza@gmail.com>
2021-11-21 14:17:03 -0600

    starting again

--- src/App.js
+++ src/App.js
@@ -1,11 +1,8 @@
 import React from 'react';
-import Layout from './components/layout/Layout'
 import './App.css';
 
 function App() {
-  return (
-    <Layout />
-  );
+  return "Hello World"
 }
 
 export default App;
